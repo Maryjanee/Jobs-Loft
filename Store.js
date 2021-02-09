f2b3eae4b644dcf28fd9c7eb17f08395b0a2c83a
Akpagbue Maryjane <akpagbuemaryjane@gmail.com>
2021-02-09 16:42:05 +0100

    Connect store to App

--- Store.js
+++
@@ -1,8 +0,0 @@
-import { createStore } from "redux";
-import thunk from "redux-thunk";
-import { applyMiddleware } from "redux";
-import {composeWithDevTools  } from "redux-devtools-extension";
-
-
-const Store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
-
