991740a454da83726cd8f8f1be3e73d0929f0af4
Akpagbue Maryjane <akpagbuemaryjane@gmail.com>
2021-02-10 16:13:10 +0100

    Add CategoryFilter and Header

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,18 @@
+import '../styles/App.scss';
+
 import {
   Switch, Route, Redirect,
 } from 'react-router-dom';
 import RecipeList from '../containers/RecipeList';
 import Recipe from '../containers/Recipe';
+import Header from './Header';
+import CategoryFilter from './CategoryFilter';
 
 const App = () => (
   <Switch>
     <div className="App">
+      <Header />
+      <CategoryFilter />
       <Route path="/" exact component={RecipeList} />
       <Route path="/recipe/:recipe" exact component={Recipe} />
       <Redirect path="/" />
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => (
+  <header>
+    <div className="container">
+      <h1>Meals Loft</h1>
+    </div>
+  </header>
+);
+
+export default Header;
